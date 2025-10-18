40449924f0fb3ccc8bb77a3e90c3252889473c69
landlucian <anndndd3@gmail.com>
2025-10-18 17:06:59 -0400

    Update engine2_dll.hpp

--- engine2_dll.hpp
+++ engine2_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-18 21:01:11.852556 UTC
+// 2025-10-18 21:06:17.197587800 UTC
 
 #pragma once
 
@@ -11,6 +11,21 @@ namespace cs2_dumper {
         // Class count: 48
         // Enum count: 2
         namespace engine2_dll {
+            // Alignment: 4
+            // Member count: 3
+            enum class EntityDormancyType_t : uint32_t {
+                ENTITY_NOT_DORMANT = 0x0,
+                ENTITY_DORMANT = 0x1,
+                ENTITY_SUSPENDED = 0x2
+            };
+            // Alignment: 4
+            // Member count: 4
+            enum class EntityIOTargetType_t : uint32_t {
+                ENTITY_IO_TARGET_INVALID = 0xFFFFFFFFFFFFFFFF,
+                ENTITY_IO_TARGET_ENTITYNAME = 0x2,
+                ENTITY_IO_TARGET_EHANDLE = 0x6,
+                ENTITY_IO_TARGET_ENTITYNAME_OR_CLASSNAME = 0x7
+            };
             // Parent: None
             // Field count: 0
             namespace EventClientPostSimulate_t {
@@ -30,7 +45,7 @@ namespace cs2_dumper {
                 constexpr std::ptrdiff_t m_nTotalTicksThisFrame = 0x38; // int32
                 constexpr std::ptrdiff_t m_nTotalTicks = 0x3C; // int32
             }
-            // Parent: None
+            // Parent: 
             // Field count: 1
             namespace CEntityIOOutput {
                 constexpr std::ptrdiff_t m_Value = 0x18; // CVariantBase<CVariantDefaultAllocator>
